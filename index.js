e3fd6574cfd2d1b7f9a328656e8d38980b514d1b
Mia <110987959+miacolonthree@users.noreply.github.com>
2024-06-01 13:26:27 +0300

    VERY stupid, but it works

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const tileSize = document.body.clientWidth > 800 ? 100 : 50;
 var col = 0,
     row = 0;
 
+var isOut = false;
+
 const wrapper = document.getElementById('tiles');
 
 const createTile = i => {
@@ -39,7 +41,7 @@ const setRadius = () => {
 	let w = document.body.clientWidth,
 	    h = document.body.clientHeight;
 
-	bg.style.setProperty('--r', `${(w > h ? w : h) * 0.15}px`);
+	bg.style.setProperty('--r', `${Math.floor((w > h ? w : h) * 0.15)}px`);
 }
 
 setRadius();
@@ -49,14 +51,40 @@ window.onresize = () => {
 	setRadius();
 }
 
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
 const handleMove = e => {
+	isOut = false;
+
 	let x = e.clientX,
 	    y = e.clientY;
 
 	bg.style.setProperty('--x', `${x}px`);
 	bg.style.setProperty('--y', `${y}px`);
+
+	bg.style.setProperty('--c', '#FFF0');
 }
 
 document.body.onmousemove = e => handleMove(e);
 
 document.body.ontouchmove = e => handleMove(e.touches[0]);
+
+const handleRelease = async () => {
+	isOut = true;
+
+	let c1 = 222,
+	    c2 = 0,
+	    t = 500;
+
+	let sleepTime = Math.floor(t / Math.abs(c1 - c2));
+
+	for (i = Math.min(c1, c2); i <= Math.floor(Math.max(c1, c2) / 5); ++i) {
+	    if (!isOut) break;
+	    await sleep(sleepTime);
+	    bg.style.setProperty('--c', `rgba(255, 255, 255, ${i / 51})`);
+	}
+}
+
+document.body.onmouseout = () => handleRelease();
+
+document.body.ontouchend = () => handleRelease();
