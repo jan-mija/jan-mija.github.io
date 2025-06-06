a2fa92700bc7120619f1a6e7521500f984fa0350
Mia <110987959+jan-mija@users.noreply.github.com>
2025-06-06 10:30:15 +0000

    undeltarune

--- time/index.js
+++ time/index.js
@@ -1,8 +1,7 @@
 //var until_ms = 1714504402000;
 //const until_ms = 1717156800000;
 //const until_ms = 1748736000000;
-const until_ms = 1749042000000; // DELTARUNE
-var isActive = true;
+var isActive = false;
 
 const main = document.getElementById('main');
 
